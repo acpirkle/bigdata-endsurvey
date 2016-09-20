96cb7a56d00d37ea32c0b1244bc2033eec784594
Andrew Pirkle <andrewcpirkle@gmail.com>
2016-09-20 16:45:58 -0400

    fixed delete button
    also added bootstrap changes

--- admin.php
+++ admin.php
@@ -1,10 +1,20 @@
 <?php session_start();
+echo $_SESSION['username'];
+if (!isset($_SESSION['username'])) {
+  header( 'Location: startpage.php');
+}
 // Do SQL Stuff
 // Creating connection to database
 $conn = new mysqli("localhost","root","password","endSurvey");
 // Check connection
 if($conn->connect_error){
   die("Connection failed: " . $conn->connect_error);
+}
+if (isset($_POST['delBttns'])) {
+  $id = $_POST['delBttns'];
+  $deletesql = "DELETE FROM endSurveyTable WHERE id = '$id'";
+  $result = $conn->query($deletesql);
+  unset($_POST['delBttns']);
 }
   if (isset($_POST['order'])) {
 
@@ -16,7 +26,7 @@ if($conn->connect_error){
 }else {
   $sql = "SELECT * FROM endSurveyTable ORDER BY time DESC";
 }
-  //$sql = "SELECT * FROM endSurveyTable ORDER BY time DESC";
+
 $result = $conn->query($sql);
 ?>
 <html>
@@ -36,25 +46,30 @@ $result = $conn->query($sql);
   <div></div>
 <div class="adminTbl">
   <table class="table table-hover">
-    <tr>
-      <th>Email</th>
-      <th>Question 1</th>
-      <th>Question 2</th>
-      <th>Question 3</th>
-      <th></th>
-    </tr>
+    <thead>
+      <tr>
+        <th>Email</th>
+        <th>Question 1</th>
+        <th>Question 2</th>
+        <th>Question 3</th>
+        <th>Time</th>
+        <th></th>
+      </tr>
+    </thead>
     <?php while ($row = $result->fetch_assoc()) : ?>
       <tr>
         <td><?php echo $row["email"]; ?></td>
         <td><?php echo $row["question1"]; ?></td>
         <td><?php echo $row["question2"]; ?></td>
         <td><?php echo $row["question3"]; ?></td>
-        <td><button type="button" onclick="alert('Hello World!')">Delete</td>
+        <td><?php echo $row["time"]; ?></td>
+        <td><form  method="Post"><button type="submit" class="btn btn-default btn-sm" name="delBttns" id="delBttns"
+          value="<?php echo $row['id'] ?>">Delete</button></form></td>
       </tr>
   <?php endwhile;
-  $conn->close();
   if (isset($_POST['logout'])) {
     session_destroy();
+    $conn->close();
     header('Location: startpage.php');
   }?>
 <form method="post">
